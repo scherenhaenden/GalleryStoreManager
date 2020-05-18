554fd229a32d9f9752370ad20ec14c462951a0cb
Eddie Krill <scherenhaenden@gmail.com>
2020-05-18 11:53:32 +0200

    Create subproject to handle file manipulations
    To get the files to be store, I would prefer to create a new project that handles everything related to raw file manipulation.
    
    -Get files from the origin paths
    -Filter by file path.

---
+++ FilesTooling/Find/ISeekFiles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FilesTooling.Find
+{
+    public interface ISeekFiles
+    {
+        string[] GetFilePathsByTargetDirectory(string path);
+
+        IEnumerable<string> GetFilePathsByTargetDirectoryFindByExtensions(string path, string[] seekedExtensions);
+    }
+}
