5ac3d4cff559748691e0fbcac39fac3dc9e1979a
Eddie Krill <scherenhaenden@gmail.com>
2020-08-30 11:22:18 +0200

    Add models Repo

---
+++ ModelsRepo/Files/GenericFiles.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ModelsRepo.Files
+{
+    public class GenericFiles
+    {
+        public string FileName;
+        public string HashValue;
+        public string CurrentPath;
+
+    }
+}
