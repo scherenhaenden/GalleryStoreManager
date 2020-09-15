38afc85acab58effa1032479c8653bf3184e5221
Eddie Krill <scherenhaenden@gmail.com>
2020-09-15 20:51:07 +0200

    Draft constants class

--- GalleryStoreManagerTests/Constans/PathForTests.cs
+++ GalleryStoreManagerTests/Constans/PathForTests.cs
@@ -1,10 +1,26 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+
 namespace GalleryStoreManagerTests.Constans
 {
-    public class PathForTests
+    public static class PathForTests
     {
-        public PathForTests()
+        public static string GetPathForTests()
         {
+            var locationOfAssembly = typeof(Tests).Assembly.Location;
+            var currentPath = Path.GetDirectoryName(locationOfAssembly);
+
+            var getPathForTests = Directory.GetDirectories(currentPath + "/../../");
+            var testPath = getPathForTests.SingleOrDefault(x => x.ToLower().Contains("testfiles"));
+            var dirs = Directory.GetDirectories(testPath);
+
+
+            var originDirectory = dirs.SingleOrDefault(x => x.ToLower().Contains("origin"));
+            var value = Directory.GetParent(originDirectory);
+
+            return value.FullName;
+
         }
     }
 }
